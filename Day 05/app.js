635d6c171cf38a96ed70ddda5450177493e2aeef
Hitesh Jain <hitesh.bafna00@gmail.com>
2021-09-20 23:06:05 +0530

    Done

---
+++ Day 05/app.js
@@ -0,0 +1,5 @@
+var Test = require("./module");
+
+var obj = new Test();
+
+console.log(obj.print());
---
+++ Day 05/module.js
@@ -0,0 +1,13 @@
+class Test{
+    print(){
+        console.log("I an inside module_test");
+    }
+}
+
+class Test2{
+    print(){
+        console.log("I an inside module_test2");
+    }
+}
+
+module.exports = Test;
