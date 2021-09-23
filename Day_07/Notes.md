3e4932f13b9ef6a19780424694f394d893af7976
Hitesh Jain <hitesh.bafna00@gmail.com>
2021-09-23 20:01:06 +0530

    Done

--- Day_07/Notes.md
+++ Day_07/Notes.md
@@ -1,3 +1,5 @@
+# Learn express and url handeling
+
 - What is package json -> this is nodejs project/ any project that use js
 - "version": "1.0.0" -> "major change.minor feature.bugs fixed" 
 - dependencies need on production level
--- Day_07/index.js
+++ Day_07/index.js
@@ -1,37 +1,58 @@
-const express = require("express")
-const app = express()
+const express = require('express');
+const app = express();
 
-app.set("view engine", "ejs")
-app.use(express.urlencoded({ extended: false }))
-app.use(express.json())
+//CRUD
+//secure, performance, edge cases (at evry line youneed to consider this)
 
-var userList = [{
-    username: 'hitesh',
-    password: 'abc'
-}]
+app.get('/', (req,res) =>{
+    // res.send is most common so for specificity we use...
+    res.status(200).json({ a:1 });  
+    res.send(); //consider as return statment!!
 
-app.get("/", (req, res) => {
-    res.render("index")
+    // res.status(200).send("Hello Boys"); -> pipeline 
+    // We can send anything using res.send(string/obj/array/a file)
 })
 
-app.post("/login", (req, res) => {
-    let user = {
-        username : req.query.username,
-        password : req.query.password
-    }
-    userList.push(user)
-
-    // Do something with this
-    res.send(user)
+// http://localhost:3000/product?text=hello
+// param is divided by <?>
+// http://localhost:3000/product?text=hello boys -> for handeling spaces we use encodeURIComponent in js
+//  
+app.get('/product',(req,res) =>{
+    res.send(req.query.text);
 })
 
-app.get("/user/:username", (req, res) => {
-    userList.forEach((user) => {
-        if(req.params.username === user.username){
-            res.send(userList)
-        }
-    })
-    res.send("Username not found")
+//if in my path b is optional then -> '/abcd' or 'acd'
+app.get('/ab?cd',(req,res)=>{
+    res.send('abcd');
 })
 
-app.listen(3000, () => console.log("Listing at http://localhost:3000"))
+//if in my path b is more than one then-> '/abbbd' or 'abbbbbbbbbbbcd'
+app.get('/ab+cd',(req,res)=>{
+    res.send('abbbbbbbbbbbbbbbbbbbbbbcd');
+})
+
+//anything after b but cd in last -> '/abhgfcd' or 'abrandomcd'
+app.get('/ab*cd',(req,res)=>{
+    res.send('abghdfjsicd');
+})
+
+//cd is option -> '/abcd' or 'acd'
+app.get('/ab(cd)?e',(req,res)=>{
+    res.send('abcd');
+})
+
+//regex 
+app.get(/.*fly$/,(req,res)=>{
+    res.send('butterfly');
+})
+
+// dynamic url
+app.get('/user/:userId/books/:bookId',(req,res)=>{
+    res.send(req.params);
+})
+
+
+
+
+
+app.listen(3000, () => console.log("Listing at http://localhost:3000"));
